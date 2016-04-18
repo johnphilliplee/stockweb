b4c43f886efb12b4d7d97330d9d6051ef0ec7790
John Phillip Lee <johnphilliplee@247-inc.com>
2016-04-18 15:00:51 +0800

    Adding web service

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,3 +9,24 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/stock/<code>')
+def price_lookup(code):
+    html = urllib.urlopen(address).read() # READ IT ALL
+    soup = BeautifulSoup(html, "html.parser")
+
+    tags = soup('span')
+    result = None
+    for tag in tags:
+        if tag.get('id', None) == 'quote_val':
+            cont = str(tag.contents)
+            result = re.findall('[0-9.]+', cont)
+
+    if result is not None:
+        data = {}
+        data[str(code)] = str(result[0])
+        return flask.jsonify(**data)
+
+    data = {}
+    data[str(code)] = "Not Found"
+    return flask.jsonify(**data)
