9c0edc258008445926b5e5c80d55fe2ae518b706
John Phillip Lee <johnphilliplee@247-inc.com>
2016-04-18 15:31:41 +0800

    Fixed libraries

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask import jsonify
 from bs4 import BeautifulSoup
+import urllib
+import re
 
 app = Flask(__name__)
 
@@ -12,21 +15,21 @@ if __name__ == '__main__':
 
 @app.route('/stock/<code>')
 def price_lookup(code):
+    address = 'http://quotes.wsj.com/PH/' + str(code)
     html = urllib.urlopen(address).read() # READ IT ALL
     soup = BeautifulSoup(html, "html.parser")
-
     tags = soup('span')
-    result = None
+
     for tag in tags:
         if tag.get('id', None) == 'quote_val':
             cont = str(tag.contents)
             result = re.findall('[0-9.]+', cont)
 
-    if result is not None:
-        data = {}
-        data[str(code)] = str(result[0])
-        return flask.jsonify(**data)
-
     data = {}
+    if result is not None:
+        data[str(code)] = str(result[0])
+        print jsonify(**data)
+        return jsonify(**data)
+
     data[str(code)] = "Not Found"
-    return flask.jsonify(**data)
+    return jsonify(**data)
